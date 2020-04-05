7d476654357a1e4088b952494da90e82c83a8012
FrancescoMussi <francesco.mussi@hotmail.it>
2020-04-05 13:05:55 +0300

    Added get account method to AccountController

--- api/app/Http/Controllers/AccountController.php
+++ api/app/Http/Controllers/AccountController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $account = Account::find($id);
+
+        if (!$account) {
+            return response('account_not_found');
+        }
+
+        $transactions = Transaction::where('to', $account->id)->orWhere('from', $account->id)->get();
+        return response()->json(compact('account', 'transactions'), 200);
+    }
 }
